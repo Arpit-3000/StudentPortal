d64be4fda13618363d28569babf78c3f5c4588f6
Arpit <Arpit123@.com>
2025-09-11 12:45:34 +0530

    url changed

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,6 @@
 // Environment Configuration
 export const config = {
-  API_BASE_URL:'http://localhost:5000',
+  API_BASE_URL:'https://jarvis-1-muxr.onrender.com',//localhost:5000',
   APP_NAME: import.meta.env.VITE_APP_NAME || 'College Portal',
   APP_VERSION: import.meta.env.VITE_APP_VERSION || '1.0.0',
 };
