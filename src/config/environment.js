aa6308e9b3f0850c5008315b826ae6c03ca1beef
Arpit Srivastava <arpitcollege1205@gmail.com>
2025-12-11 02:18:01 +0530

    Update environment.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,6 @@
 // Environment Configuration
 export const config = {
-  API_BASE_URL: 'http://localhost:5000',
+  API_BASE_URL: 'https://jarvis-liard-mu.vercel.app/',
   APP_NAME: import.meta.env.VITE_APP_NAME || 'College Portal',
   APP_VERSION: import.meta.env.VITE_APP_VERSION || '1.0.0',
   GOOGLE_CLIENT_ID: '448392427521-dv94r0ipsdipl7anb38dotnn4b0o6cu6.apps.googleusercontent.com',
